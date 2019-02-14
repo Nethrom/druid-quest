89c1bb9f19871ef1511c89e55c99038d76384a78
vkoplin <44987414+vkoplin@users.noreply.github.com>
2019-02-13 17:59:57 -0700

    Button Code

--- App.js
+++ App.js
@@ -3,17 +3,39 @@ import {
   StyleSheet,
   Text,
   ImageBackground,
-  View
+  View, 
+  Button,
+  ToggleSwitch,
 } from 'react-native';
+
 
 export default class App extends React.Component{
   render () {
     return (
+      <view>
       <ImageBackground source = {require ('./assets/ForestBG.jpg')} style = {styles.container}>
-        </ImageBackground>
-      
+      </ImageBackground>
+
+        //Start Button 
+        <Button
+        title="Start"
+        />
+          
+       //Music Toggle Button 
+      <ToggleSwitch 
+      isOn={false}
+      onColor='green'
+      offColor='red'
+      label='Music'
+      labelStyle={{color: 'black', fontWeight: '900'}}
+      size='large'
+      onToggle={ (isOn) => console.log('changed to :', isOn)}
+      />
+      </view>
     )
   }
+
+
 }
 const styles = StyleSheet.create({
   container: {
