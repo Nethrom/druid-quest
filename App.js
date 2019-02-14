bdf84f25b826d9d5f9033257dac7dac14eeeea4e
Nethrom <42158737+Nethrom@users.noreply.github.com>
2019-02-13 18:02:09 -0700

    Broken

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   ImageBackground,
   View
 } from 'react-native';
+import { Button } from 'react-native';
 
 export default class App extends React.Component{
   render () {
@@ -15,6 +16,14 @@ export default class App extends React.Component{
     )
   }
 }
+
+<Button
+  onPress={onPressLearnMore}
+  title="Quit"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
