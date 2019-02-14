fdce7d9461fe6b1c61b45224e85d19236ff7a0f8
Nethrom <42158737+Nethrom@users.noreply.github.com>
2019-02-13 19:01:18 -0700

    Cleaned up a few errors ^__^

--- App.js
+++ App.js
@@ -5,41 +5,51 @@ import {
   ImageBackground,
   View, 
   Button,
-  ToggleSwitch,
+  Switch,
 } from 'react-native';
 
 
 export default class App extends React.Component{
+  state = {
+    swtichValue: false
+  }
+  switchChange = () => {
+    this.setState({swtichValue: !this.state.swtichValue});
+  }
+  startGame = () => {
+    console.log('Start button pressed')
+  }
   render () {
     return (
-      <view>
-      <ImageBackground source = {require ('./assets/ForestBG.jpg')} style = {styles.container}>
-      </ImageBackground>
-
-        //Start Button 
+      <View style={styles.container}>
+        <ImageBackground source={require ('./assets/ForestBG.jpg')} style={styles.bg} >
+        </ImageBackground>
+ 
         <Button
         title="Start"
+        onPress={this.startGame}
         />
-          
-       //Music Toggle Button 
-      <ToggleSwitch 
-      isOn={false}
-      onColor='green'
-      offColor='red'
-      label='Music'
-      labelStyle={{color: 'black', fontWeight: '900'}}
-      size='large'
-      onToggle={ (isOn) => console.log('changed to :', isOn)}
-      />
-      </view>
+        
+        <Switch onValueChange={this.switchChange} value={this.state.swtichValue} />
+      
+      </View>
     )
   }
 
+
+  
 
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bg: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
