f50d72aa40f098233a99ebb89da95312009a70d9
Nethrom <42158737+Nethrom@users.noreply.github.com>
2019-03-02 20:02:52 -0700

    All updates

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,5 +1,5 @@
 return (
-    <ImageBackground source={require('C:\Users\paswe\Downloads\DQAssets\ForestBG.jpg')} style={{width: '100%', height: '100%'}}>
-      <Text>Inside</Text>
-    </ImageBackground>
-  );
+  <ImageBackground source={require('C:\Users\paswe\Downloads\DQAssets\ForestBG.jpg')} style={{width: '100%', height: '100%'}}>
+    <Text>Inside</Text>
+  </ImageBackground>
+);
