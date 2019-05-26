4966620b8117f91035e6dbab861cc1d1c17d0441
vkoplin <44987414+vkoplin@users.noreply.github.com>
2019-05-26 16:07:40 -0600

    Attempt at Combat View Function Component

--- src/components/GameScreen/CombatView.js
+++ src/components/GameScreen/CombatView.js
@@ -6,7 +6,7 @@ import Monster from '../../models/Monster';
 
 const MAX_MONSTER_COUNT = 5;
 
-export default class CombatView extends React.Component {
+export default function CombatView() {
     // add monsters here as sub components 
     // pass in monster power/type/etc based on character data from props
     //const animalImages = [10];
@@ -15,8 +15,7 @@ export default class CombatView extends React.Component {
       monsters: [],
     }
 
-    componentDidMount() {
-      const { character } = this.props;
+      const { character } = props;
       const { level } = character;
 
       const monsterCount = level * 2;
@@ -32,8 +31,6 @@ export default class CombatView extends React.Component {
       this.setState({ monsters });    
     }    
        
-    render() {
-      const { monsters } = this.state;
         
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -44,8 +41,8 @@ export default class CombatView extends React.Component {
           }
         </View>
       );
-    }
-  }
+    
+  
 
 const styles = StyleSheet.create({
   imageStyle: {
